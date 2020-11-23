a4be9457ecfb7b46435135ba98de82025a68bdf0
NTroknya <troknya.2@wright.edu>
2020-11-23 11:38:02 -0500

    added comments to helpfully describe what is going on closes issue #3

--- ChatClient.java
+++ ChatClient.java
@@ -1,19 +1,21 @@
+//Java libraries needed for use
 import java.net.*;
 import java.util.Scanner;
 import java.io.*;
 
 public class ChatClient extends Thread {  
-
+	//Fields for the ChatClient
 	private Socket socket;
 	private Scanner userIn;
 	private PrintWriter serverOut;
 	private Scanner serverIn;
 	private String name;
 
+	//ChatClient constructor, takes in the name for the user
 	public ChatClient(String name) {  
 
 		try {
-
+			//Connects to the server
 			socket = new Socket("cs.hopku.net", 14445);
 			userIn = new Scanner(System.in);
 			serverOut = new PrintWriter(socket.getOutputStream(), true);
@@ -21,23 +23,24 @@ public class ChatClient extends Thread {
 			this.name = name;
 
 		} catch (Exception e) {
+			//Prints out exception and what line it occured on
 			e.printStackTrace();
 		}
 	}
 
-
+	//Listens for messages to send to server
 	public void run() {  
 		while (true) {
 			send();
 		}
 	}
 
-
+	//Sends messages to the server
 	public void send() {  
 		serverOut.println(name + ": " + userIn.nextLine());
 	}
 
-
+	//Outputs messages from the server
 	public void update() {
 		String message = serverIn.nextLine();
 		if (!message.startsWith(name))
@@ -48,7 +51,7 @@ public class ChatClient extends Thread {
 	public static void main(String args[]) {
 		ChatClient client = new ChatClient(args[0]);
 		client.start();
-		
+		//Constatly updates the output for the user
 		while (true) {
 			client.update();
 		}
--- ChatServerThread.java
+++ ChatServerThread.java
@@ -1,19 +1,20 @@
+//Java libraries needed for use
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class ChatServerThread extends Thread {  
-
+	//ChatServerThread fields
 	private Scanner in;
 	private PrintWriter out;
 
-
+	//ChatServerThread constructor to take in messages and send them
 	public ChatServerThread(Scanner in, PrintWriter out) {  
 		this.in = in;
 		this.out = out;
 		this.start();
 	}
 
-
+	//Takes in a String and sends it to the output
 	public void send(String msg) {   
 		try {  
 			out.println(msg);
